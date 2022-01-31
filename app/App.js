c9efcf6dc7af71e70919f0cf12fa399b4c72b444
Sergey Cherkashin <cherkashin@vuflya.ru>
2022-02-01 02:14:39 +0300

    feat: add basic infrastructure

---
+++ app/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, StatusBar, View, StyleSheet } from 'react-native'
+import auth from '@react-native-firebase/auth'
+
+import ChatScreen from './screens/chatScreen'
+import LoginScreen from './screens/loginScreen'
+
+const App = () => {}
+
+export default App
