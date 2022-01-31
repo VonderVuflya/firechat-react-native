8c8694b4aeee43bc3b68ca1047ba218c86e0f53c
Sergey Cherkashin <cherkashin@vuflya.ru>
2022-02-01 02:13:47 +0300

    chore: fix linting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 module.exports = {
-  bracketSpacing: false,
+  bracketSpacing: true,
   jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'all',
   arrowParens: 'avoid',
-};
+  jsxSingleQuote: true,
+  semi: false,
+  singleQuote: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+}
